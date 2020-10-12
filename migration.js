c880834426e5062078c9df45c4ab6d46587c0c6d
Oleksii Makodzeba <oleksiim@wix.com>
2020-10-12 15:46:49 +0300

    make it simple

--- migration.js
+++ migration.js
@@ -1,13 +1,10 @@
 try {
-  window.autopilot.log("start script");
+  //window.autopilot.log("start script");
 
   const isPublished = window.autopilot.isPublishedRevision;
 
-  const componentId = window.autopilot.payload.componentId;
-  const value = window.autopilot.payload.value;
-
-  window.autopilot.log("updating component label");
-  window.autopilot.log("end script");
+  //window.autopilot.log("updating component label");
+  //window.autopilot.log("end script");
 
   window.autopilot.reportResult(
     `This was a simple jsonp migration on the ${
